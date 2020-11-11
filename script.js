eabcdc6a85301542b3268319ebc496511edff83d
shobhithk <shobhithkamata01@gmail.com>
2020-11-12 00:01:12 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//loading
+
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//hide loading
+function complete(){
+    if(!loader.hidden){
+        quoteContainer.hidden=false;
+        loader.hidden = true;
+    }
+}
+
+//Get quote from API
+async function getQuote() {
+    loading();
+
+    const proxyURL = "https://cors-anywhere.herokuapp.com/"
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json' ;
+
+    try{
+        const response = await fetch(proxyURL + apiURL);
+        const data = await response.json();
+        if(data.quoteAuthor==="")
+            authorText.innerText="unknown"
+        else
+            authorText.innerText = data.quoteAuthor; 
+       if(data.quoteText.length > 120 ){
+           quoteText.classList.add("long-quote");
+       }
+       else{
+        quoteText.classList.remove("long-quote");
+       }
+            quoteText.innerText = data.quoteText;
+        //stop loader
+        complete();
+
+
+    } catch(error){
+    getQuote();
+   
+    
+    }
+
+}
+
+//tweet quote
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterURL, '_blank' );
+}
+
+// eventlisteners
+
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click",tweetQuote );
+
+//OnLoad
+getQuote();
